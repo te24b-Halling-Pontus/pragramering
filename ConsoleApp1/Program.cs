f117ad2b8d8f23d9c325b7a1de2d56c0eee33c9e
te24b-Halling-Pontus <pontushalling08@gmail.com>
2025-08-26 14:14:53 +0200

    Add initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿// string name;
+
+// Console.WriteLine("vad heter du?");
+// name = Console.ReadLine();
+// string posetivtord = "snälla";
+
+// Console.WriteLine("hej hej");
+// Console.WriteLine("Mvh " + name);
+// Console.WriteLine($"idag tycker {name} att ni är {posetivtord} idag");
+
+
+
+// Console.ReadLine();
+
+
+using System.Runtime.CompilerServices;
+
+string name;
+
+Console.WriteLine("vad heter du?");
+name = Console.ReadLine();
+
+string favorit_mat;
+string viktig_del;
+string fokusera;
+string tilbehor;
+Console.WriteLine("vad är din favorit mat");
+favorit_mat = Console.ReadLine();
+Console.WriteLine("vad är den viktigaste delen i din" + favorit_mat);
+viktig_del = Console.ReadLine();
+Console.WriteLine("vad är det man ska fokusera på för att få det perfekta " + viktig_del);
+fokusera = Console.ReadLine();
+Console.WriteLine("vad är det bästa tilbehöret t.ex ketscup på carbonara");
+tilbehor = Console.ReadLine();
+
+Console.WriteLine($"Den bästa maten i värden är {favorit_mat}. Hur skulle man kunna leva utan {favorit_mat}? Den bästa och viktigaste delen av en {favorit_mat} är {viktig_del} ");
+
+
